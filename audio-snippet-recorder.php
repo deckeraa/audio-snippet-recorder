20a1e864abee3ae3e4f0a084c0b4e8f393747e09
Aaron Decker <decker49@gmail.com>
2020-05-25 06:06:47 -0500

    Add [snippets] shortcode.

--- audio-snippet-recorder.php
+++ audio-snippet-recorder.php
@@ -3,3 +3,14 @@
  * Plugin Name: Audio Snippet Recorder
  */
 
+function snippets_shortcode($atts = [], $content = null, $tag = '') {
+   $o = '';
+   $o .= '<h2>Audio Snippet Recorder</h2>';
+   return $o;
+}
+
+function snippets_shortcodes_init() {
+   add_shortcode('snippets', 'snippets_shortcode');
+}
+
+add_action('init', 'snippets_shortcodes_init');
